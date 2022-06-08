737b96cb329a284a5a71ce55d3e4d22402c62ecd
Roman.Campillo <tumacat@gmail.com>
2022-06-09 01:14:21 +0200

    primera version

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-document.write("<h1>Web Solora</h1>")
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.cards', { delay: 500 });
